cd4a24f89fe46d3ce309b29fdcf5dfb1d3a3c3fc
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2021-03-21 17:29:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
