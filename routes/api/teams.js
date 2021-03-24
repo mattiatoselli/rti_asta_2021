148c1296e26914498e172db05d8e708e54f99e6d
mattiatoselli <44556380+mattiatoselli@users.noreply.github.com>
2021-03-24 09:12:46 +0100

    Update teams.js

--- routes/api/teams.js
+++ routes/api/teams.js
@@ -7,8 +7,22 @@ const teamsNames = ["Racing Team Italia", "Volanti ITR", "MySubito Casa", "A24",
 
 //list team infos
 router.get("/", async (req,res)=>{
-        const teams = await loadTeamsCollection();
-        res.send(await teams.find({}).toArray());
+        const uri = "mongodb+srv://rti_user:rti@astaRti2021.dbx5j.mongodb.net/rti_db?retryWrites=true&w=majority";
+        // Create a new MongoClient
+        const client = new MongoClient(uri);
+        try {
+                // Connect the client to the server
+                await client.connect();
+                // Establish and verify connection
+                const teams = await client.db("rti_db").collection("teams");
+                res.send(await teams.find({}).toArray());
+                //console.log("Connected successfully to server");
+        } finally {
+                // Ensures that the client will close when you finish/error
+                await client.close();
+        }
+       // const teams = await loadTeamsCollection();
+       // res.send(await teams.find({}).toArray());
 });
 
 
